99927d6fdcbde854ca39d181e2f7365d7093570f
Davis Byamugisha <davisb256@gmail.com>
2021-10-22 13:38:44 +0300

    Rename file touches file and run python script to wrtie csv

--- src/Davis_CollectFiles.py
+++ src/Davis_CollectFiles.py
@@ -59,15 +59,15 @@ repo = 'scottyab/rootbeer'
 # Remember to empty the list when going to commit to GitHub.
 # Otherwise they will all be reverted and you will have to re-create them
 # I would advise to create more than one token for repos with heavy commits
-lstTokens = ["fd02a694b606c4120b8ca7bbe7ce29229376ee",
-                "16ce529bdb32263fb90a392d38b5f53c7ecb6b",
-                "8cea5715051869e98044f38b60fe897b350d4a"]
+
+lstTokens = ["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"]
 
 dictfiles = dict()
 countfiles(dictfiles, lstTokens, repo)
 print('Total number of files: ' + str(len(dictfiles)))
 
 file = repo.split('/')[1]
+
 # change this to the path of your file
 fileOutput = 'data/file_' + file + '.csv'
 rows = ["Filename", "Touches"]
